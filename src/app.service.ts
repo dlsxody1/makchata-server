cc454116be83c3af314444a81bef29787396fdae
김인태 <dlsxody1@naver.com>
2023-10-23 17:06:26 +0900

    modify

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,37 +5,43 @@ import { ConfigService } from '@nestjs/config';
 export class AppService {
   constructor(private configService: ConfigService) {}
   async getCurrentDirection(latitude: string, longitude: string): Promise<any> {
-    const res = await fetch(
-      `https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?coords=${longitude},${latitude}&output=json&orders=addr,admcode`,
-      {
-        mode: 'cors',
-        headers: new Headers({
-          'X-NCP-APIGW-API-KEY-ID': this.configService.get<string>('CLIENT_ID'),
-          'X-NCP-APIGW-API-KEY':
-            this.configService.get<string>('CLIENT_SECRET'),
-        }),
-      },
-    );
-    const currentLocation = await res.json();
-    const mergeCurrentLocation = `${currentLocation?.results[0]?.region.area1.name} ${currentLocation.results[0].region.area2.name} ${currentLocation?.results[0]?.region.area3.name} ${currentLocation?.results[0]?.region.area4.name}${currentLocation?.results[0]?.land.number1}-${currentLocation?.results[0]?.land.number2}`;
-    const data = await fetch(
-      `https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=${mergeCurrentLocation}`,
-      {
-        mode: 'cors',
-        headers: new Headers({
-          'X-NCP-APIGW-API-KEY-ID': this.configService.get<string>('CLIENT_ID'),
-          'X-NCP-APIGW-API-KEY':
-            this.configService.get<string>('CLIENT_SECRET'),
-        }),
-      },
-    );
-    const result = await data.json();
+    try {
+      const res = await fetch(
+        `https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?coords=${longitude},${latitude}&output=json&orders=addr,admcode`,
+        {
+          mode: 'cors',
+          headers: new Headers({
+            'X-NCP-APIGW-API-KEY-ID':
+              this.configService.get<string>('CLIENT_ID'),
+            'X-NCP-APIGW-API-KEY':
+              this.configService.get<string>('CLIENT_SECRET'),
+          }),
+        },
+      );
+      const currentLocation = await res.json();
+      const mergeCurrentLocation = `${currentLocation?.results[0]?.region.area1.name} ${currentLocation?.results[0]?.region.area2.name} ${currentLocation?.results[0]?.region.area3.name} ${currentLocation?.results[0]?.region.area4.name}${currentLocation?.results[0]?.land.number1}-${currentLocation?.results[0]?.land.number2}`;
+      const data = await fetch(
+        `https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=${mergeCurrentLocation}`,
+        {
+          mode: 'cors',
+          headers: new Headers({
+            'X-NCP-APIGW-API-KEY-ID':
+              this.configService.get<string>('CLIENT_ID'),
+            'X-NCP-APIGW-API-KEY':
+              this.configService.get<string>('CLIENT_SECRET'),
+          }),
+        },
+      );
+      const result = await data.json();
 
-    return {
-      currentLocation: result?.addresses[0].roadAddress,
-      x: result?.addresses[0].x,
-      y: result?.addresses[0].y,
-    };
+      return {
+        currentLocation: result?.addresses[0].roadAddress,
+        x: result?.addresses[0].x,
+        y: result?.addresses[0].y,
+      };
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   async getCurrentLocation(search: string, departure: string): Promise<any> {
